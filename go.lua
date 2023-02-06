094401828f1d5d5264105d750231b2f7d0ca6790
Therk <43972622+Therrk@users.noreply.github.com>
2023-02-06 10:38:38 -0500

    fixed string causing error

--- go.lua
+++ go.lua
@@ -4,7 +4,7 @@ local buffer = import("micro/buffer")
 function onSave(bp)
 	if bp.Buf:fileType()=="tex" then
 		filename = bp.Buf:GetName()
+		shell.RunCommand("xdg-open " .. string.sub(filename,1,#filename-3) .. "pdf |grep \"^\./\"" .. filename .."tex")
 		shell.RunCommand("pdflatex -interaction=nonstopmode ".. filename)
-		shell.RunCommand("xdg-open " .. string.sub(filename,1,#filename-3) .. "pdf"|grep "^\./" .. filename .."tex")
 	end
 end
