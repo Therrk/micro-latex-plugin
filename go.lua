87db24c59e6e72d0e75c73198695feae64509698
Therk <43972622+Therrk@users.noreply.github.com>
2023-02-08 11:45:30 -0500

    added support for launching pdf viewer on micro launch Also, micro doesn't hang when the pdf viewer is running

--- go.lua
+++ go.lua
@@ -4,7 +4,30 @@ local buffer = import("micro/buffer")
 function onSave(bp)
 	if bp.Buf:fileType()=="tex" then
 		filename = bp.Buf:GetName()
-		shell.RunCommand("xdg-open " .. string.sub(filename,1,#filename-3) .. "pdf |grep \"^\./\"" .. filename .."tex")
-		shell.RunCommand("pdflatex -interaction=nonstopmode ".. filename)
+		filename = string.sub(filename,1,#filename-4)
+		compile(filename)
+		display(filename)
 	end
+end
+
+function onBufPaneOpen(bp)
+	if bp.Buf:fileType() == "tex" then
+		local filename = bp.Buf:GetName()
+		filename = string.sub(filename,1,#filename-4)
+		compile(filename)
+		display(filename)
+	end
+end
+
+function compile(filename)
+	shell.ExecCommand("bash", "-c", "pdflatex -interaction=nonstopmode -file-line-error " .. filename .. ".tex | grep \"^\\./\"", "&")
+	-- return compil_errors
+end
+
+function display(filename)
+	shell.JobStart("xpdf -rv " .. filename .. ".pdf", test,test,test, nil)
+	-- return display_errors
+end
+
+function test(std, userargs)
 end
